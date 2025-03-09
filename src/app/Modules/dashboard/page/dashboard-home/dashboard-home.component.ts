3fd90580ab5e27ad810d7913fcb93256db69a56a
lfbandeira <121176457+lfbandeira@users.noreply.github.com>
2025-03-09 20:15:24 -0300

    Tela Principal e responsividade

--- src/app/Modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/Modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Funcionalidade } from 'src/app/interfaces/Funcionalidade';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard-home.component.css']
 })
 export class DashboardHomeComponent {
+    funcionalidades:Funcionalidade[] = [
+      {
+      id:27,
+      desc_funcionalidade:"Cadastrar Perfil",
+      link_pagina:"PerfilList",
+      source_icone:"https://raw.githubusercontent.com/lfbandeira/public/refs/heads/main/Perfil.png"
+    },
+    {
+      id:28,
+      desc_funcionalidade:"Atribuir Perfil",
+      link_pagina:"AtribuirPerfil",
+      source_icone:"https://github.com/lfbandeira/public/blob/main/AtribuirPerfil.png?raw=true"
+    },
+    {
+      id:53,
+      desc_funcionalidade:"Noticias",
+      link_pagina:"ListaNoticias",
+      source_icone:"https://github.com/lfbandeira/public/blob/main/Cadastrar%20Noticias.png?raw=true"
+    },
+    {
+      id:53,
+      desc_funcionalidade:"Condomínios",
+      link_pagina:"CondominioList",
+      source_icone:"https://github.com/lfbandeira/public/blob/main/buildings.png?raw=true"
+    },
+    {
+      id:25,
+      desc_funcionalidade:"Funcionalidades",
+      link_pagina:"FuncionalidadeList",
+      source_icone:"https://github.com/lfbandeira/public/blob/main/Funcionalidade.png?raw=true"
+    },
 
-}
+  ]
+  }
---
+++ src/app/interfaces/Funcionalidade.ts
@@ -0,0 +1,7 @@
+export interface Funcionalidade {
+  id:number;
+  desc_funcionalidade:string;
+  link_pagina :string;
+  source_icone:string;
+
+}
